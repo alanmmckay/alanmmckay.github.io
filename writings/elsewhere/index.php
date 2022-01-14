04e732c888c47a95af44baba4bf2d4a22d9c6634
alanmmckay <alanmmckay@gmail.com>
2022-01-13 23:07:39 -0600

    New Article: Experiencing Elsewhere

---
+++ writings/elsewhere/index.php
@@ -0,0 +1,170 @@
+<?php
+
+$normalize = '../../normalize.css';
+
+$style = '../../style.css';
+
+$canonical = 'http://alanmckay.blog/writings/elsewhere/';
+
+$title = 'Alan McKay | Writing | Experiencing Elsewhere';
+
+$meta['title'] = 'Alan McKay | Experiencing Elsewhere';
+
+$meta['description'] = '...';
+
+$meta['url'] = 'http://alanmckay.blog/writings/elsewhere/';
+
+include('../../header.php');
+
+?>
+
+<section id='writingsWrapper'>
+            <section>
+                <article>
+                    <header>
+                    <h1>
+                        Experiencing Elsewhere
+                    </h1>
+                    </header>
+                   <p>
+                        A wintery haze shrouded the top of the Black Hills as the car wound the roads towards our stepping off point for the day's hike. There always has been a distaste for a commute by car; let alone one which is necessary for the adventure of the day. The automobiles' cabin serves to isolate the occupants from both the world and the occupants of other cabins; those who are also on their own predetermined paths. These paths once represented adventure but are now made trivial by the ease and isolation of modern travel. With this context, it is natural to think that the shrouded mountain tops provide another means of isolation that leaves the local world feeling even smaller.
+                    </p>
+                    <p>
+                        Luckily, the goal was to peer into this veil and reestablish a true sense of scale; to nullify the triviality of the initial commute. Immediately, stepping away from the now settling car started to instill this real sense of scale and the sense of being within the world. This is in spite of the fact that we entered the dense pine forest as the encasing clouds started to part. The canopy of the forest now obscures our vision but our existence outside of the cabin enables our other senses to make up for this deficiency by helping us feel a part of something greater.
+                    </p>
+                    <p>
+                        The steady gain of altitude into the forested hills was representative of the steady increase in conversation and normal hiking banter as the senses lost their sleepiness. Trudging on the ground and through the occasional pockets of snow, one individual decides to veer off the marked path towards a raised bluff. The other two follow.
+                    </p>
+                    <br>
+                    <hr>
+                    <a href='../../images/black-hills-silhouette.jpg' target="_blank" rel="noopener noreferrer">
+                        <figure>
+                            <img src='../../images/black-hills-silhouette.jpg' id='black-hills-silhouette' alt="A silhouetted man stepping to peer through winter forest foliage." style='transition:object-position 4s;width:100%;height:400px;object-fit:cover;object-position:0 20%;' />
+                            <figcaption>
+                                Clayton stepping up to peer out of the forest line.
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <hr>
+                    <br>
+                    <p>
+                        Conversation ceased once the bluff was crested. The forest dipped down into the contour of the valley before them, which then raised back up onto the hills adjacent. The now less obscured sense of vision witnessing that the once lingering clouds had deposited moisture onto the needles which have frozen and  been lightly dusted with a minor flurry.
+                    </p>
+                    <br>
+                    <hr>
+                    <a href='../../images/black-hills-trees.jpg' target="_blank" rel="noopener noreferrer">
+                        <figure>
+                            <img src='../../images/black-hills-trees.jpg' id='black-hills-trees' alt="Frosted pine along the contour of a hill" style='transition:object-position 9s;width:100%;height:400px;object-fit:cover;object-position:0 0%;'/>
+                            <figcaption>
+                                The frosted pine following the ridgelines of the Black Hills.
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <hr>
+                    <br>
+                    <p>
+                        One individual reaffirms in the others that it was indeed beautiful.
+                    </p>
+                    <p>
+                        Back on the path, the relation of altitude and conversation is maintained. The temperature drops as this relation strengthens. The ice on the trees becomes more vivid along with the increasing jaggedness of the exposed rock in the hills.
+                    </p>
+                    <!--hr>
+                    <a href='../../images/black-hills-tree.jpg' target="_blank" rel="noopener noreferrer">
+                        <figure>
+                            <img src='../../images/black-hills-tree.jpg' id='black-hills-tree' alt='' style='transition:object-position 9s;width:100%;height:400px;object-fit:cover;object-position:0 0%;'/>
+                            <figcaption>
+
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <hr-->
+                    <p>
+                        The three eventually reach the peak of the hike. Displayed before them a sea of pine dusted white moving along the hills toward the horizon.
+                    </p>
+                    <p>
+                        Despite the cold, they linger. The choir of the crisp pop produced by the opening of canned beverages briefly fills the air. These sounds exist in order to continue the tradition of enjoying a can of Hamm's on every peak that is summited. At this time, travel is no longer happening as a true sense of scale is being fully appreciated.
+                    </p>
+                    <br>
+                    <hr>
+                    <a href='../../images/black-hills-peak.jpg' target="_blank" rel="noopener noreferrer">
+                        <figure>
+                            <img src='../../images/black-hills-peak.jpg' id='black-hills-peak' alt="Two men face away, overlooking a summit. A sea of frosted pine flow down into the valley adjacent." style='transition:object-position 4s;width:100%;height:400px;object-fit:cover;object-position:55%;'>
+                            <figcaption>
+                                Clayton and Michael overlooking the peak of the day's hike.
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <hr>
+                    <br>
+                    <p>
+                        In the moment, the height of the hike has been obtained; the adventure has been completed. An alternate route is to be taken to the exit vehicle. The three descend the summit through winding paths that snake around the hills in a similar manner of the roads that took them to this experience.
+                    </p>
+                    <p>
+                        The relationship of conversation and altitude stays true. Conversation hushes as the group descends towards the finish. Nothing needs to be said.
+                    </p>
+                    <br>
+                    <hr>
+                    <a href='../../images/black-hills-path.jpg' target="_blank" rel="noopener noreferrer">
+                        <figure>
+                            <img src='../../images/black-hills-path.jpg' id='black-hills-path' alt="The silhouette of a man traversing through a pine forest." style='width:100%;height:400px;object-fit:cover;'/>
+                            <figcaption>
+                                The sharp sunlight of a setting sun emphasizes the ordinary pine in the lower altitude of the descent.
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <hr>
+                    <br>
+                    <p>
+                        The pine no longer glistens in the sun. The white is now back to the default green. The sharp angle of the winter sunlight is now being blocked by the hills as the sun nears the horizon. This induces a melancholy that harshens conversation further.
+                    </p>
+                    <p>
+                        The last jog of the exit hike winds down into a valley between two dominant hills. The car is waiting at a lot for a county park which sits next to a frozen lake. The two hills at the side of the lake are hidden in the shadow of the hill behind us. A third hill on the far side of the lake is illuminated by the sharp sunlight.
+                    </p>
+                    <p>
+                        It is clear we had not previously reached the apex moment of the hike. In the distance, at the base of the illuminated hill, sits a tent. Silhouettes dance around a net a short distance from this net. The sound of skates scraping against the ice, the wooden clashing of hockey sticks, and the sound of the puck sliding reaches the ears of the three hikers. These sounds echo through the valley and are accented by the playful murmur of the small group across the ice. Two ice fishers associated with the tent are idle. Their attention is on the spectacle of the scene before us all.
+                    </p>
+                    <br>
+                    <hr>
+                    <a href='../../images/black-hills-lake.jpg' target="_blank" rel="noopener noreferrer">
+                        <figure>
+                            <img src='../../images/black-hills-lake.jpg' alt="A frozen lake with a ice fishing tent and a game of hockey being played upon it." />
+                            <!--figcaption>
+                                The lake at the end of the hike.
+                            </figcaption-->
+                        </figure>
+                    </a>
+                    <p>
+                        One individual affirms in the others that it was indeed beautiful.
+                    </p>
+                    <hr>
+                </article>
+                <nav>
+                    <a href='../'>Back</a>
+                </nav>
+            </section>
+        </section>
+    </body>
+    <script>
+        //Abstract this function to include range parameters
+        function inViewRange(elementID,inPosition,outPosition){
+            element = document.getElementById(elementID);
+            bounding = element.getBoundingClientRect();
+            if ( ((window.innerHeight/2) - bounding.top > 0) && (bounding.top > 0) )
+            {
+                element.style['object-position'] = inPosition;
+            } else{
+                element.style['object-position'] = outPosition;
+            }
+        }
+        window.onscroll = function(){
+            inViewRange('black-hills-peak','0 20%','0 55%');
+            inViewRange('black-hills-silhouette','0 0%','0 20%');
+            inViewRange('black-hills-trees','0 75%','0 0%');
+
+        }
+
+        inViewRange('black-hills-peak','0 20%','0 55%');
+        inViewRange('black-hills-silhouette','0 0%','0 20%');
+        inViewRange('black-hills-trees','0 75%','0 0%');
+    </script>
+</html>
