b85999e31bee1983d8a6173f03e030b99b79a2c6
Alan McKay <51932760+alanmmckay@users.noreply.github.com>
2022-12-16 17:43:51 -0600

    Staging php page for new writing - Database Extension Gauging Cycling Safety

---
+++ writings/safety/index.php
@@ -0,0 +1,55 @@
+<?php
+
+$normalize = '../../normalize.css';
+
+$style = '../../style.css';
+
+$canonical = 'http://alanmckay.blog/writings/safety/';
+
+$title = 'Alan McKay | Writing | Database Extension Gauging Cycling Safety';
+
+$meta['title'] = 'Alan McKay | Database Extension Gauging Cycling Safety';
+
+$meta['description'] = "";
+
+$meta['url'] = 'http://alanmckay.blog/writings/safety/';
+
+include('../../header.php');
+
+?>
+        <section id='writingsWrapper'>
+            <section>
+                <article>
+                    <section class='info'>
+                        <header>
+                            <h2>Preface</h2>
+                        </header>
+                        <p>
+
+                        </p>
+                    <hr>
+                    </section>
+                    <header>
+                    <h1>
+                        Database Extension Gauging Cycling Safety
+                    </h1>
+                    </header>
+                    <p>
+
+                    </p>
+                    <section class='info'>
+                        <hr>
+                        <h3>Concluding notes</h3>
+                        <p>
+
+                        </p>
+                        <hr>
+                    </section>
+                </article>
+                <nav>
+                    <a href='../'>Back</a>
+                </nav>
+            </section>
+        </section>
+    </body>
+</html>
