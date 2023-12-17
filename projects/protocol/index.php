33fac39fa2b195a241694f758f55bac9e9938cf1
Alan <alanmmckay@gmail.com>
2023-12-17 04:27:43 +0000

    Integrated diagrams and added new styling for aside images

--- projects/protocol/index.php
+++ projects/protocol/index.php
@@ -87,7 +87,10 @@ include('../../header.php');
                         A bird’s sensor switches from L to H when it’s temperature reaches the threshold. When it interacts with another bird, it will then communicate this fact. The following is one possible set of interactions a group of  5 birds may exhibit when one has a status of H:
                     </p>
                     <figure>
-
+                        <img src="../../images/pp_A.png" style="max-width:400px;"/>
+                        <figcaption style='max-width:450px;margin:auto;margin-top:1%;'>
+                            Figure A.
+                        </figure>
                     </figure>
                     <p>
                         Note the above set of interactions is a subset of a set of potential interactions. Within this superset exists an infinite amount of sets which may contain an infinite amount of interactions. The fifth step highlights this reason by showing two agents have interacted with each other redundantly. That is, the interaction has no bearing on progressing the state of the population to convergence. It is by the convergence of a solution enforced by a loose fairness condition that these algorithms are evaluated and developed.
@@ -101,44 +104,55 @@ include('../../header.php');
                         <p>
                             What if a population wants to report if a certain percentage meets a threshold? One approach is to allow each agent access to 3 finite state machines. One to track a numerator, another to track a denominator, and the third as a boolean value to determine if an agent has already been evaluated for computation.
                         </p>
-                        <ul>
-                            <li>Q = {L -/-, H -/-, 0 n/m, 1 x/y}  where n,m,x, y are Integers. </li>
-                            <li>Σ = {L, H}</li>
-                            <li>ι(x) = x</li>
-                            <li>ω(0 n/m) = ω(1 n/m) = n/m</li>
-                            <li>δ = { <ul>
-                                <li>(H -/-,H -/-) -> (2/2, 2/2),</li>
-                                <li>(L -/-, H -/-) -> (1/2, 1/2),</li>
-                                <li>(H -/-, L -/-) -> (1/2, 1/2),</li>
-                                <li>(L -/-, L -/-) -> (0/2, 0/2),</li>
-                                <li>(0 n/m, 0 x/y) -> (0 (n+x)/(m+y),  (1 (n+x)/(m+y)),</li>
-                                <li>(0 n/m, 1 x/y) -> (0 n/m, 1 n/m)</li> </ul>
-                                        } </li>
-                        </ul>
-                        <p>
-                            This Algorithm can be used in conjunction with leader election and epidemics[3][4] to ensure data is ready to be collected.  Once it is, the collector simply has to check the given ratio.
-                        </p>
+                        <div class='aside'>
+                            <figure class='narrow'>
+                                <img src='../../images/pp_B.png' style='width:95%;max-width:150px'/>
+                                <figcaption>
+                                    Figure B.
+                                </figure>
+                            </figure>
+                            <ul>
+                                <li>Q = {L -/-, H -/-, 0 n/m, 1 x/y}  where n,m,x, y are Integers. </li>
+                                <li>Σ = {L, H}</li>
+                                <li>ι(x) = x</li>
+                                <li>ω(0 n/m) = ω(1 n/m) = n/m</li>
+                                <li>δ = { <ul>
+                                    <li>(H -/-,H -/-) -> (2/2, 2/2),</li>
+                                    <li>(L -/-, H -/-) -> (1/2, 1/2),</li>
+                                    <li>(H -/-, L -/-) -> (1/2, 1/2),</li>
+                                    <li>(L -/-, L -/-) -> (0/2, 0/2),</li>
+                                    <li>(0 n/m, 0 x/y) -> (0 (n+x)/(m+y),  (1 (n+x)/(m+y)),</li>
+                                    <li>(0 n/m, 1 x/y) -> (0 n/m, 1 n/m)</li> </ul>
+                                            } </li>
+                            </ul>
+                            <p>
+                                This Algorithm can be used in conjunction with leader election and epidemics[3][4] to ensure data is ready to be collected.  Once it is, the collector simply has to check the given ratio.
+                            </p>
+                        </div>
                         <hr>
                     </section>
                     <p>
                         To help better understand the concept as a whole, we have built a simulator which keeps track of the iterations of a protocol’s execution while monitoring relevant data such as redundant interactions that may contribute to developing more complex algorithms for this model. Figures B and C represent a heatmap of redundant interactions between agents of populations of 5 agents and 15 agents, respectively. Figures A, B, and C are all graphs output by the simulator we have constructed.
                     </p>
                     <h2> Future and Application </h2>
+                    <div class='aside'>
+                    <figure class='narrow'>
+                        <img src="../../images/pp_C.png" style='max-width:200px;'/>
+                        <figcaption>
+                            Figure C.
+                        </figcaption>
+                    </figure>
                     <p>
                         With the simulator we’ve built, the goal is to be able to implement and test a suite of population protocols to explore fault tolerance. Heatmap output should help us hone in on any potential bottlenecks caused by more advanced algorithms involving agents with differing roles.
                     </p>
 
-                    <hr>
-                    <figure>
-                        <img>
-                        <figcaption>
-                            Figure C: This specific heat map shows the set of agents and each potential path of interaction.  Higher intensity of color between a pair of agents indicates a greater amount of redundant interactions between the two. This emphasizes the fact that an agent’s failure may cause problems in terms of the execution of the protocol as it’s information may be critical for convergence.
-                        </figcaption>
-                    </figure>
-                    <hr>
+                    <p>
+                        This specific heat map (Figure C) shows the set of agents and each potential path of interaction.  Higher intensity of color between a pair of agents indicates a greater amount of redundant interactions between the two. This emphasizes the fact that an agent’s failure may cause problems in terms of the execution of the protocol as it’s information may be critical for convergence.
+                    </p>
                     <p>
                         Exploration of this computational model will allow us to make the most of the primitive computational power afforded by simple hardware. This can be extended to automated drone communication. Beyond development of the simulator, we would like to develop and test practical application with the usage of drones and drones communicating with sensory networks.
                     </p>
+                    </div>
                     <h2>References</h2>
                     <ul>
                         <li>[1] Angluin, Dana ; Aspnes, James ; Diamadi, Zoë ; Fischer, Michael ; Peralta, René. Computation in networks of passively mobile finite-state sensors. Distributed Computing, 2006, Vol.18(4), pp.235-253</li>
