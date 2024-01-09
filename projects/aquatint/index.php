f2490d7c918975befaa7751ed5bbdd107c903adc
Alan <alanmmckay@gmail.com>
2024-01-09 02:16:05 +0000

    Initial push for a writeup about the aquatint image processor

---
+++ projects/aquatint/index.php
@@ -0,0 +1,84 @@
+<?php
+
+$normalize = '../../normalize.css';
+
+$style = '../../style.css';
+
+$canonical = 'https://alanmckay.blog/projects/aquatint/';
+
+$title = 'Alan McKay | Project | Aquatint';
+
+$meta['title'] = 'Alan McKay | Aquatint';
+
+$meta['description'] = '';
+
+$meta['url'] = 'https://alanmckay.blog/projects/aquatint/';
+
+$relative_path = "../../";
+
+include('../../header.php');
+
+?>
+        <section id='writingsWrapper'>
+            <section>
+                <article>
+                    <section class='info'>
+                        <header>
+                            <h2>Preface</h2>
+                        </header>
+                        <p>
+                            An appreciated facet of my life has been the happenstance connections I've made with a wide range of individuals. Despite having studied computer sciences for so long, most friendships and acquaintances were formed outside of the domain. This has been a strong theme in many of my writings, which often revolve around experiencing moments with this ever growing assorted group of individuals. During my graduate years, effort was spent casting a wider net.
+                        </p>
+
+                        <p>
+                            The casting of the social net would land upon the university's bouldering wall. One thing that impressed me about the wall were the people who attended it. If one were to walk through the rec center a glance to the right would show isolated individuals trying their best to ignore each other taking silent turns taking advantage of equipment made for individual use. A glance to the left was stark; a climbing wall with a vibrant splay holds and groups of people accompanied by a positive murmur of discussion. Highlights of discussion include group strategy for approaching a given route and cheerful encouragement of one who was about finish a particular one.
+                        </p>
+
+                        <p>
+                            Discussion would typically occur amongst pockets of individuals who were college aged - early to mid 20s. Fortunately, this wasn't a rule. Being a non-traditional student in their early 30s, it was easy for me to integrate. Considering social norms, I could understand how it would be a bit daunting to step into - especially if one were to look at the opposite side of the rec center. For this reason, I was sure to put effort forth to reciprocate the ease of experiencing the social perks of the climbing wall.
+                        </p>
+
+                        <p>
+                            One such individual met through these circumstances was someone who seemed to be from a generation or two beyond me; an older gentleman who seemed to be learning the art of bouldering whilst enjoying the benefits the body-weight exercise and the associated skills of body awareness. Conversation was spurred by discussing a how to climb particular route.
+                        </p>
+
+                        <p>
+                            Turns out this individual was a physics professor. Continued discussion would reveal that he was looking for a software engineer to make a user interface wrapper for an image processing script he had finished making. Providence would have it that I'm a computer scientist with a good amount of experience in software development with user interface design.
+                        </p>
+
+                        <p>
+                            The script would reprocess an image to one that had the appearance of being produced using the aquatint printmaking technique. The product would be the same image with a more old-time aesthetic to it; the image would be more grainy with a warm temperature reminiscent of early photography.
+                        </p>
+
+                        <p>
+                            The professor's script, through the web app I would end up building, would be used to produce three pieces of building art for the Physics Research Center at the University of Chicago. It's fitting that a serendipitous connection at a climbing wall would manifest on a wall at an educational institution elsewhere.
+                        </p>
+
+                        <p>
+                            The main body of this page discusses three primary facets of the web app that I find interesting: handling of the original script, upload security, and supplying feedback to a user.
+                        </p>
+                    <hr>
+                    </section>
+                    <header>
+                        <h1></h1>
+                    </header>
+                    <p>
+
+                    </p>
+                    <section class='info'>
+                        <hr>
+                        <h3>Concluding notes</h3>
+                        <p>
+
+                        </p>
+                        <hr>
+                    </section>
+                </article>
+                <nav>
+                    <a href='../'>Back</a>
+                </nav>
+            </section>
+        </section>
+    </body>
+</html>
+
