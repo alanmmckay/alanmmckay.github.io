bc5f4e13babe971d1e82fac0350839fe4d30cc8b
Alan <alanmmckay@gmail.com>
2024-03-06 05:13:52 +0000

    More changes to keep the longer 'photography' phrase in check in terms of word-wrapping.

--- index.php
+++ index.php
@@ -16,23 +16,23 @@ include('header.php');
             <nav>
                 <a href='writings/'>
                     <img src='images/text-logo-grey.png' alt='Icon for blog link'>
-                    &nbsp;Writings
+                    <span style='margin-left:10px;'>Writings</span>
                 </a>
                 <a href='projects/'>
                     <img src='images/description.svg' alt='Icon for projects link'>
-                    &nbsp;Projects
+                    <span style='margin-left:10px;'>Projects</span>
                 </a>
-                <a href='photography/'>
+                <a href='photography/' style='overflow-wrap: break-word;hyphens: manual;'>
                     <img src='images/shutter-cut.png' alt='Icon for a camera shutter'>
-                    &nbsp;Photography
+                    <span style='margin-left:10px;'>Photo&shy;graphy</span>
                 </a>
                 <a href='https://github.com/alanmmckay' target="_blank" rel="noopener noreferrer">
                     <img src='images/github-logo-grey.png' alt='Icon for Github Link'>
-                    &nbsp;GitHub
+                    <span style='margin-left:10px;'>GitHub</span>
                 </a>
                 <a href='https://www.linkedin.com/in/alan-mckay-701b4a1b8/' target="_blank" rel="noopener noreferrer">
                     <img src='images/linkedin-logo-grey.png' alt='Icon for linked-in Link'>
-                    &nbsp;LinkedIn
+                    <span style='margin-left:10px;'>LinkedIn</span>
                 </a>
             </nav>
         </div>
