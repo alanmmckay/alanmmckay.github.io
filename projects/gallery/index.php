6b65e03931e883eae2ed158947b4d36d00a370f1
Alan <alanmmckay@gmail.com>
2024-04-01 05:26:53 +0000

    Initial setup for a writing for the image gallery

---
+++ projects/gallery/index.php
@@ -0,0 +1,55 @@
+<?php
+
+$normalize = '../../normalize.css';
+
+$style = '../../style.css';
+
+$canonical = 'https://alanmckay.blog/projects/gallery/';
+
+$title = 'Alan McKay | Project | Image Gallery';
+
+$meta['title'] = 'Alan McKay | Image Gallery';
+
+$meta['description'] = '';
+
+$meta['url'] = 'https://alanmckay.blog/projects/gallery/';
+
+$relative_path = "../../";
+
+include('../../header.php');
+
+?>
+        <section id='writingsWrapper'>
+            <section>
+                <article>
+                    <section class='info'>
+                        <header>
+                            <h2>Preface</h2>
+                        </header>
+                        <p>
+
+                        </p>
+                    <hr>
+                    </section>
+                    <header>
+                        <h1>Javascript: Image Gallery</h1>
+                    </header>
+                    <p>
+
+                    </p>
+                    <section class='info'>
+                        <hr>
+                        <h3>Concluding notes</h3>
+                        <p>
+
+                        </p>
+                        <hr>
+                    </section>
+                </article>
+                <nav>
+                    <a href='../'>Back</a>
+                </nav>
+            </section>
+        </section>
+    </body>
+</html>
