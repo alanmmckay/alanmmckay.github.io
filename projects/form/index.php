e43e94d4c29b63f7858a8f32d5e912146b43ed0b
Alan <alanmmckay@gmail.com>
2024-06-02 18:40:29 +0000

    Inclusion of more code blocks for projects/form. Started adding figcaptions while setting max-height properties for iframes.

--- projects/form/index.php
+++ projects/form/index.php
@@ -40,17 +40,23 @@ include('../../header.php');
                         The Rails framework for Ruby acts as a layer of abstraction to help expedite the development process of building web applications using the Model-View-Controller software pattern. It takes a programming-by-convention approach in which the mere existence of a given object will infer the existence of some other object or subroutine without the developer's explicit declaration of said other object or subroutine. These implicit connections are often scaffolded by a generative commands within the framework where a developer can explicitly declare a model or controller name and the framework stubs the required files and routes for the framework to operate on.
                     </p>
                     <figure class='code-figure'>
-                        <iframe frameborder="0" style='width:100%;max-height:150px;overflow:auto' max-height='150' src='code/01.php'></iframe>
+                        <iframe frameborder="0" style='width:100%;overflow:auto;max-height:95px' max-height='95' src='code/01.php'></iframe>
+                        <figcaption>Example of a scaffold command.</figcaption>
                     </figure>
                     <p>
                         The usage of these commands and the subsequent stubbing of files doesn't necessarily expose the implicit logic. A good example of this is the mere existence of a some model and a generic route for the model allows for the usage of an implicit variable that acts as a URL path for some action to take. Generically, if some <code>&lt;model&gt;</code> exists, and a <code>&lt;route&gt;</code> for the model is declared in the routes configuration file, then the framework allows a developer to leverage a reference of <code>&lt;model&gt;_&lt;route&gt;_path</code> which will tell a given controller action to take a user the indicated controller action.
                     </p>
+                    <hr><br>
                     <figure class='code-figure'>
-                        <iframe frameborder="0" style='width:100%;max-height:150px;overflow:auto' max-height='150' src='code/02.php'></iframe>
+                        <iframe frameborder="0" style='width:100%;overflow:auto;max-height:305px' max-height='305' src='code/02.php'></iframe>
+                        <figcaption>Declaration of a route within <code>config/routes.rb</code>.</figcaption>
                     </figure>
+                    <br>
                     <figure class='code-figure'>
-                        <iframe frameborder="0" style='width:100%;max-height:150px;overflow:auto' max-height='150' src='code/03.php'></iframe>
+                        <iframe frameborder="0" style='width:100%;overflow:auto;max-height:255px' max-height='255' src='code/03.php'></iframe>
+                        <figcaption>Usage of implicit <code>users_new_path</code> within a view.</figcaption>
                     </figure>
+                    <hr>
                     <p>
                         This programming-by-convention approach requires a developer to be keen on the implications and how they approach to leverage and/or scaffold features of the framework. Deciding to generate a model where the developer gives a name that's plural, for example, is an example where convention can cause pain. Consider the above example in reference to <code>users_create_path</code>. When the user model was generated, the singular form of the noun was given. From it, the plural form of the noun was used to build a controller and a view. What occurs when the plural of user is given during generation? While pondering this question, the reader is invited to look into the <code>--force-plural</code> flag for the rails generate command whilst also considering the <a href='https://api.rubyonrails.org/classes/ActiveSupport/Inflector/Inflections.html' target="_blank" rel="noopener noreferrer"><code>ActiveSupport::Inflector::Inflections</code></a> class.
                     </p>
@@ -71,13 +77,18 @@ include('../../header.php');
                         Consider a registration form for a web app. The information submitted through this form is applied to a user model. This is established through the users controller by taking the new action. The model, controller, and route is as declared as follows:
                     </p>
                     <figure class='code-figure'>
-                        <iframe frameborder="0" style='width:100%;max-height:150px;overflow:auto' max-height='150' src='code/04.php'></iframe>
+                        <iframe frameborder="0" style='width:100%;overflow:auto;max-height:820px' max-height='820' src='code/04.php'></iframe>
+                        <figcaption>Full definition of a <code>User</code> model within <code>app/models/user.rb</code>.</figcaption>
                     </figure>
+                    <br>
                     <figure class='code-figure'>
-                        <iframe frameborder="0" style='width:100%;max-height:150px;overflow:auto' max-height='150' src='code/05.php'></iframe>
+                        <iframe frameborder="0" style='width:100%;overflow:auto;max-height:1270px' max-height='1270' src='code/05.php'></iframe>
+                        <figcaption>Partial definition of the <code>UsersController</code> within <code>app/controllers/users_controller.rb</code>.</figcaption>
                     </figure>
+                    <br>
                     <figure class='code-figure'>
-                        <iframe frameborder="0" style='width:100%;max-height:150px;overflow:auto' max-height='150' src='code/06.php'></iframe>
+                        <iframe frameborder="0" style='width:100%;overflow:auto;max-height:335px' max-height='335' src='code/06.php'></iframe>
+                        <figcaption>Introduction of <code>users_create_path</code> within <code>config/routes.rb</code>.</figcaption>
                     </figure>
                     <p>
                         The controller action operates on a hash map arbitrarily called info. info is filled by a function called user_params. Here, the require and permit methods are chained to produce a one dimensional hash-map which contains a set of keys associated with the symbols provided in the permit method. Both the require and permit methods include validation features abstracted away from the developer, in addition to the production of the hash-maps used within the action controller.
@@ -86,7 +97,7 @@ include('../../header.php');
                         These chained methods assume the existence of a hash-map of a certain structure. This hash-map reflects what is posted to the controller action. Based on these method calls and the user model, it can be assumed that a user's email, password, a password confirmation, first name, last name, and phone number should be posted through an HTML form. The Rails framework expects the posted form data to be in a hash-map within <code>params[:user]</code>. <code>params</code> contains all post information, which can encapsulate information pertaining to different models. An input field which posts to <code>&lt;model&gt;[&lt;attribute&gt;]</code> will be placed into <code>params[:&lt;model&gt;][:&lt;attribute&gt;]</code>. Knowing this, an initial template can be built.
                     </p>
                     <figure class='code-figure'>
-                        <iframe frameborder="0" style='width:100%;max-height:150px;overflow:auto' max-height='150' src='code/07.php'></iframe>
+                        <iframe frameborder="0" style='width:100%;overflow:auto;max-height:1085px' max-height='1085' src='code/07.php'></iframe>
                     </figure>
                     <p>
                         This above template is the view which will be presented should the new controller action be invoked. Submission of this form will invoke the create controller action through an HTTP post. This template is close to minimum in terms of interacting with the Rails framework. These interactions are highlighted, and can also be noticed by the usage of the <code>&lt;% %&gt;</code> tags.
@@ -102,7 +113,7 @@ include('../../header.php');
                         The natural evolution of this template will introduce the usage of built-in helpers such as those included in <code><a href='https://api.rubyonrails.org/classes/ActionView/Helpers.html' target="_blank" rel="noopener noreferrer">ActionView::Helpers</a></code>. Specifically, the <code>FormHelper</code> and <code>FormTagHelper</code> namespaces. Specifically <code>form_tag</code>, <code>label_tag</code>, and <code>text_field</code> are used.
                     </p>
                     <figure class='code-figure'>
-                        <iframe frameborder="0" style='width:100%;max-height:150px;overflow:auto' max-height='150' src='code/08.php'></iframe>
+                        <iframe frameborder="0" style='width:100%;overflow:auto;max-height:1060px' max-height='1060' src='code/08.php'></iframe>
                     </figure>
                     <p>
                         The <code>form_tag</code> helper puts into place the <code>&lt;form&gt;</code> opening and closing tags while also placing a couple of hidden <code>input</code> fields which take care of the session token authentication previously discussed. The <code>label_tag</code> helper creates a relevant set of <code>&lt;label&gt;</code> tags while the <code>text_field</code> helper creates a relevant set of <code>input</code> tags whose <code>type</code> is set to text. It should be observed that the initial assignment of <code>model</code> has been turned into a <code>symbol</code> as a means to adhere to the conventions set place within the documentation of these methods. That is, the <code>text_field</code> expects a <code>symbol</code> to be supplied as an argument for its <code>object</code> and <code>method</code> parameters. This is contrary to the <code>label_tag</code> helper which expects a <code>string</code>.
@@ -113,21 +124,22 @@ include('../../header.php');
                     <p>
                         What is the advantage of using these helpers? They begin to abstract away the syntax required of HTML. One needs not worry about closing open HTML tags for each call. Possible mistakes of typing a tag's attributes and their properties are mitigated; no longer does a developer need to type out <code>class="..."</code>, <code>placeholder="..."</code>, <code>for="..."</code>, <code>id="..."</code>, etc. The aspects of markup that are important, such as assigning a class to an element, are isolated for the developer.
                     </p>
+                    <h4>Introducing HAML</h4>
                     <p>
-                        This approach doesn't completely eliminate these syntactic aspects. The template includes a set of div and header tags that are vulnerable to being opened but not closed. The helper methods themselves need to be enclosed in a set of tags which act as a mechanism for the <code>ERB</code> template to know that a Ruby expression should be evaluated. How can these HTML elements be abstracted away?
+                        The above approach doesn't completely eliminate these syntactic aspects. The template includes a set of div and header tags that are vulnerable to being opened but not closed. The helper methods themselves need to be enclosed in a set of tags which act as a mechanism for the <code>ERB</code> template to know that a Ruby expression should be evaluated. How can these HTML elements be abstracted away?
                     </p>
                     <p>
                         Luckily, such a technology already exists. <code><a href="https://haml.info/"  target="_blank" rel="noopener noreferrer">HAML</a></code> was created precisely for these reasons:
                     </p>
                     <figure class='code-figure'>
-                        <iframe frameborder="0" style='width:100%;max-height:150px;overflow:auto' max-height='150' src='code/09.php'></iframe>
-                        <iframe frameborder="0" style='width:100%;max-height:150px;overflow:auto' max-height='150' src='code/10.php'></iframe>
+                        <iframe frameborder="0" style='width:100%;overflow:auto;max-height:280px' max-height='280' src='code/09.php'></iframe>
+                        <iframe frameborder="0" style='width:100%;overflow:auto;max-height:280px' max-height='280' src='code/10.php'></iframe>
                     </figure>
                     <p>
                         HAML stands for HTML Abstraction Markup Language. Usage of HAML using Ruby on Rails requires its gem to be bundled with the project. Doing so will allow a controller to route to a view whose extension is <code>.html.haml</code> instead of <code>.html.erb</code>. Here, the markup abstractions can be leveraged to clean up view code and prevent common mistakes that come from code reuse of literal strings. Converting the template to HAML will produce the following template:
                     </p>
                     <figure class='code-figure'>
-                        <iframe frameborder="0" style='width:100%;max-height:150px;overflow:auto' max-height='150' src='code/11.php'></iframe>
+                        <iframe frameborder="0" style='width:100%;overflow:auto;max-height:795px' max-height='795' src='code/11.php'></iframe>
                     </figure>
                     <section class='info'>
                         <hr>
