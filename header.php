be8550876eb47eb225b26620a6294c8b658eb3f3
Alan <alanmmckay@gmail.com>
2024-06-16 22:46:16 +0000

    New value for stylesheet to force a cache reset.

--- header.php
+++ header.php
@@ -39,7 +39,7 @@ if(!isset($relative_path)){
 }
 
 if(!isset($style)){
-    $style = $relative_path.'style.css?v=050924';
+    $style = $relative_path.'style.css?v=061424';
 }
 
 if(!isset($normalize)){
