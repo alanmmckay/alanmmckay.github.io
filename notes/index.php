9275247f51a32a7bc29aabf9d576003d24b2685b
Alan <alanmmckay@gmail.com>
2024-10-14 16:27:20 +0000

    Index page for new notes section. Requires a set of json files to properly build. These files tell the script where to direct a user to view relevant markdown note. Unsure if these will be published on github.

---
+++ notes/index.php
@@ -0,0 +1,113 @@
+<?php
+
+$canonical = 'https://alanmckay.blog/projects/';
+
+$title = 'Alan McKay | Projects';
+
+$meta['title'] = 'Alan McKay | Projects';
+
+$meta['description'] = "Index of articles detailing various programming projects worked on. All related to Computer Science from the context of assessment, freelance, and/or interest.";
+
+$meta['url'] = 'https://alanmckay.blog/projects/';
+
+$relative_path = "../";
+
+include('../header.php');
+
+$json_str = file_get_contents("catalog.js");
+$json = json_decode($json_str,true);
+
+?>
+        <div id='homeWrapper'>
+            <header id='breadNav' style='overflow:hidden'>
+                <h1><a href='./' class='currentLink'>&nbsp;&gt; Notes</a></h1>
+                <h1><a href='../'>Home</a></h1>
+            </header>
+            <nav>
+                <div class='currentLink'>
+                    <a href='./'>
+                        <img src='../images/notes.svg' alt='Icon for blog link'>
+                        Notes
+                    </a>
+                </div>
+<?php
+    foreach($json as $key){
+        if(is_array($key)){
+            /* Schema:
+             * {
+             *   "location":String,
+             *   "name":String,
+             *   "date_published":String,
+             *   "description":String,
+             *   "tags":Array
+             * }
+            */
+            $info = $key;
+            $key = $info['location'];
+        }else{
+            $info_str = file_get_contents($key."/meta.js");
+            $info = json_decode($info_str,true);
+        }
+?>
+                <div class='writing'>
+                    <a href="<?php echo $key ?>/" style='display:inherit;'>
+                        <span><?php echo $info['name'] ?></span>
+                        <p>
+                            <?php echo $info['description'] ?>
+                        </p>
+                    </a>
+<?php
+        if(isset($info['tags']) && count($info) > 0){
+?>
+                    <ul>
+<?php
+            foreach($info['tags'] as $tag){
+?>
+                        <li><a><?php echo $tag; ?></a></li>
+<?php
+            }
+?>
+                    </ul>
+<?php
+        }
+?>
+                    <p><?php echo $info['date_published']; ?></p>
+                </div>
+<?php
+    }
+?>
+            </nav>
+        </div>
+    </body>
+    <script src='../js/index_functions.js' ></script>
+    <script>
+
+        var isMobile = window.matchMedia || window.msMatchMedia;
+        isMobile = isMobile("(pointer:coarse)").matches;
+
+        if(isMobile){
+
+            let scroll_end = null;
+
+            window.onscroll = function(ev){
+                if(scroll_end != null){clearTimeout(scroll_end);}
+                applyClassTransitionEffects('writing', 'border-left', 'solid white 10px', '.5s', 'solid #778088 2px', '1s', 35);
+                scroll_end = setTimeout(function(){
+                    primeClassTransitions("writing","border-left","solid 2px","2s",true);
+                },750);
+            }
+
+            window.onscrollend = function(ev){
+                primeClassTransitions("writing","border-left","solid 2px","2s",true);
+            }
+
+            window.addEventListener('load', function () {
+                setTimeout(function(){
+                    primeClassTransitions("writing","border-left","solid 2px","2s",false);
+                },100);
+            });
+
+        }
+
+    </script>
+</html>
