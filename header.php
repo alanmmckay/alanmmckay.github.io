0988f2cc4bd635e2c48f5a8f29b7b4747e7fb060
Alan <alanmmckay@gmail.com>
2025-04-21 00:14:04 +0000

    Forgot to update the header in tandem with the stylesheet from the last commit to force a cache reset.

--- header.php
+++ header.php
@@ -39,7 +39,7 @@ if(!isset($relative_path)){
 }
 
 if(!isset($style)){
-    $style = $relative_path.'style.css?v=101424.2';
+    $style = $relative_path.'style.css?v=042025';
 }
 
 if(!isset($normalize)){
